a5c1c899364f10baa4aa28a6a131a969765492ac
AdriLighting <grellard.adrien@gmail.com>
2022-01-13 12:20:59 +0100

    upd

--- src/bp_debouce.h
+++ src/bp_debouce.h
@@ -33,7 +33,7 @@
  */
 
 #ifndef BP_DEBOUCE_H
-#define BP_DEBOUCE_H
+#define BP_DEBOUCE_H  
 
 #include <Arduino.h>
 #include <adri_timer.h>
@@ -41,15 +41,15 @@
 // #define DEBUG
 #ifdef DEBUG
 
-static const char debug_shortPress 				[] PROGMEM = "shortPress";
-static const char debug_longPress 				[] PROGMEM = "longPress";
-static const char debug_longPressHandle 		[] PROGMEM = "longPress callback";
-static const char debug_longPressReleased 		[] PROGMEM = "longPress released";
+static const char debug_shortPress        [] PROGMEM = "shortPress";
+static const char debug_longPress         [] PROGMEM = "longPress";
+static const char debug_longPressHandle   [] PROGMEM = "longPress callback";
+static const char debug_longPressReleased [] PROGMEM = "longPress released";
 
-static const char p_bpd_pin 		[] PROGMEM = "pin";
-static const char p_bpd_pullup 		[] PROGMEM = "pullup";
-static const char p_bpd_input 		[] PROGMEM = "input";
-static const char p_bpd_statu 		[] PROGMEM = "statu";
+static const char p_bpd_pin       [] PROGMEM = "pin";
+static const char p_bpd_pullup    [] PROGMEM = "pullup";
+static const char p_bpd_input     [] PROGMEM = "input";
+static const char p_bpd_statu     [] PROGMEM = "statu";
 
 #define fsget(parm_a) String(FPSTR(parm_a))
 
@@ -57,20 +57,20 @@ static const char p_bpd_statu 		[] PROGMEM = "statu";
 
 enum mBPD
 {
-	mBPD_inactive,
-	mBPD_raised,
-	mBPD_detect_long,
-	mBPD_detect_short,
-	mBPD_long_end,
+  mBPD_inactive,
+  mBPD_raised,
+  mBPD_detect_long,
+  mBPD_detect_short,
+  mBPD_long_end,
 };
 enum mBPDc
 {
-	mBPDc_click_1,
-	mBPDc_click_2,
-	mBPDc_click_3,
-	mBPDc_longCick_stard,
-	mBPDc_longCick_end,
-	mBPDc_none,
+  mBPDc_click_1,
+  mBPDc_click_2,
+  mBPDc_click_3,
+  mBPDc_longCick_stard,
+  mBPDc_longCick_end,
+  mBPDc_none,
 
 };
 // region ################################################ BPDEBOUNCE
@@ -79,90 +79,90 @@ private:
 
 
 public:
-	int 		_pin = -1;
-	boolean 	_pullup = true;
-	uint8_t 	_input;
+  int     _pin = -1;
+  boolean   _pullup = true;
+  uint8_t   _input;
 
-	bpDebounce(int pin, boolean pullup, uint8_t mode);
+  bpDebounce(int pin, boolean pullup, uint8_t mode);
 
     boolean statuGet();
-    void 	statuGet(boolean & result);	
-    void 	pinGet(int & result);
-    void 	pullupGet(boolean & result);
-    void 	inputGet(uint8_t & result);
-};	
+    void  statuGet(boolean & result); 
+    void  pinGet(int & result);
+    void  pullupGet(boolean & result);
+    void  inputGet(uint8_t & result);
+};  
 // endregion >>>> BPDEBOUNCE
 
 // region ################################################ BPDEBOUNCE_CALLBACK
 typedef void (*_BP_callbackFunc)();  
 class bpDebounce_callback {
 private:
-	mBPDc 				_mod		= mBPDc_none;
-	_BP_callbackFunc	_func 		= NULL;
-	boolean 			_activate 	= true;	
+  mBPDc         _mod    = mBPDc_none;
+  _BP_callbackFunc  _func     = NULL;
+  boolean       _activate   = true; 
 public:
 
-	bpDebounce_callback(_BP_callbackFunc * func);
-	void activate(boolean & ret);
-	void toglle();
-	void func();
-	void func_change(_BP_callbackFunc * func);
-};	
+  bpDebounce_callback(_BP_callbackFunc * func);
+  void activate(boolean & ret);
+  void toglle();
+  void func();
+  void func_change(_BP_callbackFunc * func);
+};  
 // endregion >>>> BPDEBOUNCE_CALLBACK
 
 // region ################################################ BPDEBOUNCEHANDLE
 class bpDebounceHandle {
 private:
-	bpDebounce_callback * _callback_sp_1 = nullptr;
-	bpDebounce_callback * _callback_sp_2 = nullptr;
-	bpDebounce_callback * _callback_sp_3 = nullptr;
-	bpDebounce_callback * _callback_lp_l = nullptr;
-	bpDebounce_callback * _callback_lp_e = nullptr;
+  bpDebounce_callback * _callback_sp_1 = nullptr;
+  bpDebounce_callback * _callback_sp_2 = nullptr;
+  bpDebounce_callback * _callback_sp_3 = nullptr;
+  bpDebounce_callback * _callback_lp_l = nullptr;
+  bpDebounce_callback * _callback_lp_e = nullptr;
 
 
-	uint32_t 	_longPress_delay 	= 3000;
-	boolean 	_longPress 			= false;
+  uint32_t  _longPress_delay  = 3000;
+  boolean   _longPress        = false;
 
-	uint8_t 	_shortPress_delay 	= 255;
-	uint8_t 	_shortPress_count	= 0;
-	uint8_t 	_shortPress			= 0;
+  uint8_t   _shortPress_delay   = 255;
+  uint8_t   _shortPress_count   = 0;
+  uint8_t   _shortPress         = 0;
 
-	boolean 	_bPread;
-	mBPD 		_statu 				= mBPD_inactive;
-	uint32_t	_lastChange;
+  boolean   _bPread;
+  mBPD      _statu = mBPD_inactive;
+  uint32_t  _lastChange;
 
-	boolean 	_reset 				= false;
-	adri_timer 	* _raisedTimer;
+  boolean     _reset = false;
+  adri_timer  * _raisedTimer;
 
 public:
-	bpDebounce * _bpDebounce;
+  bpDebounce * _bpDebounce;
 
 
-	bpDebounceHandle(bpDebounce * ptr) ;
-	void loop();
-	void reset_short();
-	void reset_long();
-	void callback(mBPDc mod, _BP_callbackFunc func);
-	void callback_toglle(mBPDc mod);
-	void callback_isRegister(mBPDc mod, boolean & result);
-	void callback_isActivate(mBPDc mod, boolean & result);
-	
-};	
+  bpDebounceHandle(bpDebounce * ptr) ;
+  void loop();
+  void reset_short();
+  void reset_long();
+  void callback(mBPDc mod, _BP_callbackFunc func);
+  void callback_toglle(mBPDc mod);
+  void callback_isRegister(mBPDc mod, boolean & result);
+  void callback_isActivate(mBPDc mod, boolean & result);
+  
+};  
 // endregion >>>> BPDEBOUNCEHANDLE
 
 // region ################################################ BPDEBOUNCE_MANAGEMENT
 class bpDebounce_management {
 private:
-	int _pos = 0;
+  int _pos = 0;
 public:
-	bpDebounce_management();
-	void addBp(int pin, boolean pullup, uint8_t mode, int & result);
-	void loop();
-	void callback(int bp, mBPDc mod, _BP_callbackFunc func);
-	void callback_toglle(int bp, mBPDc mod);
-	void print(int bp);
-	void print();
-};	
+  bpDebounce_management();
+  void addBp(int pin, boolean pullup, uint8_t mode, int & result);
+  void loop();
+  void callback(int bp, mBPDc mod, _BP_callbackFunc func);
+  void callback_toglle(int bp, mBPDc mod);
+  void print(int bp);
+  void print();
+};  
 // endregion >>>> BPDEBOUNCE_MANAGEMENT
 
 
